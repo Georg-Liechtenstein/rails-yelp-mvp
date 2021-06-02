3f5b4ab2cfa6919ca4621c961d648a753023f4cc
Georg Liechtenstein <liechtensteingeorg@gmail.com>
2021-06-02 15:05:17 +0200

    pushing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# for each category of the array CATEGORY = %w(chinese italian japanese french belgian) do
+# create a Restrautent where the category is the category just created
+
+
+# CATEGORY = %w(chinese italian japanese french belgian)
+
+Restaurant.destroy_all
+
+# 10.times do
+#   Restaurant.create(
+#     name: Faker::Restaurant.name,
+#     address: Faker::Address.street_address,
+#     phone_number: Faker::PhoneNumber.phone_number,
+#     category: Restaurant::CATEGORY.sample
+#   )
+# end
+
+Restaurant::CATEGORY.each do |category|
+  10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: category
+  )
+  end
+end
+puts "Created #{Restaurant.count} restaurants"
+
